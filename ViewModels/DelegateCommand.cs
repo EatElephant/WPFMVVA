a3e895d10ebc0c3f4112292050eae281f0f7d5cb
Bailin Work Laptop <bailin.github@gmail.com>
2016-01-11 16:35:02 -0500

    Jan 11, 2016 By Brian: Initial Commit For Backend GUI

---
+++ ViewModels/DelegateCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BackendGUI.ViewModels
+{
+    class DelegateCommand : ICommand
+    {
+        private Action _action;
+        private Func<bool> _validation;
+
+        public DelegateCommand(Action action, Func<bool> validation = null)
+        {
+            _action = action;
+            _validation = validation;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_validation == null)
+                return true;
+            else
+                return _validation();
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        public event EventHandler CanExecuteChanged { add { } remove { } }
+    }
+}
